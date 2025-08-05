93ba0caea5f9c9a389a0351c9192fc0cf6dc3862
Matthew <mstormo314@gmail.com>
2025-08-05 09:30:47 -0700

    @MS Added clock startup. Note: System no longer works at this commit. Build system has an issue with the clock running. Updating to STM32 VS Code extensions

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
+// Include standard libraries
 #include <stdio.h>
 #include <stddef.h>
 
+// Include STM32L driver libraries
+#include "stm32l4xx_hal.h"
+
+// Include all other customer headers
 #include "gpio.h"
 #include "led.h"
 #include "push_button.h"
+
+static void MAIN_vInit(void);
+static void SystemClock_Config(void);
+static void Error_Handler(void);
 
 int main(void) {
     
@@ -11,6 +20,11 @@ int main(void) {
         setvbuf(stdout, NULL, _IONBF, 0);
         printf("Hello from STM32!\n");
     #endif
+
+    HAL_StatusTypeDef status = HAL_Init();
+    SystemClock_Config();
+
+    MAIN_vInit();
 
     while (1) {
         // Toggle a pin or just loop
@@ -30,3 +44,64 @@ void MAIN_vInit(void) {
 // void SystemInit(void) {
 //     // Clock setup would go here
 // }
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
